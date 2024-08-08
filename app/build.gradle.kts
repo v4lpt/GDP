7f681769a1fc2b6deb13334ba5a1adbc6ab2b242
v4lpt <v4lpt@posteo.net>
2024-08-08 08:54:03 +0200

    fastlane update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "v4lpt.vpt.f003.gdp"
         minSdk = 24
         targetSdk = 34
-        versionCode = 101
-        versionName = "1.0.1"
+        versionCode = 103
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
